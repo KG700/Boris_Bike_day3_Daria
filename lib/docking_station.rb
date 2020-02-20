7a868e8427d2914609d150d55647156972c828b2
dariathompson <daria.kambalina@gmail.com>
2020-02-20 16:24:38 +0000

    The Single Responsibility Principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,14 +9,23 @@ class DockingStation
 
   def dock(bike)
     #@bikes.push(bike)
-    fail "Sorry, no spaces available" if @bikes.length > 19
+    fail "Sorry, no spaces available" if full?
     @bikes.push(bike)
   end
 
   def release_bike
     # should fail if no bikes are available
-    fail "Error: there are no bikes available" if @bikes.length < 1
+    fail "Error: there are no bikes available" if empty?
     @bikes.pop
+  end
+
+  private
+  def empty?
+    @bikes.length < 1
+  end
+  
+  def full?
+    @bikes.length > 19
   end
 
 end
